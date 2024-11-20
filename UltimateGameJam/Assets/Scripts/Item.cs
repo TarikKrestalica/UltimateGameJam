d05a65df3d522ed4b17e6975f8572447123a0533
Austin-bryan <austinw.bryan@yahoo.com>
2024-11-20 15:23:54 -0500

    Fixed wall offset bug

--- UltimateGameJam/Assets/Scripts/Item.cs
+++ UltimateGameJam/Assets/Scripts/Item.cs
@@ -6,37 +6,46 @@ using UnityEngine.EventSystems;
 public class Item : MonoBehaviour
 {
     [Range(0, 40f)]
-    [SerializeField] float rotSpeed;
+    [SerializeField] private float rotSpeed;
 
-    bool hasClicked = false;
+    private bool hasClicked = false;
+    private Vector3 offset; 
+
     void Update()
     {
-        if(!hasClicked)
+        if (!hasClicked)
             return;
-            
+
+        // Handle rotation
         Vector3 curRot = this.transform.localEulerAngles;
-        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             curRot.z -= Time.deltaTime * rotSpeed;
         }
-        else if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             curRot.z += Time.deltaTime * rotSpeed;
         }
 
         this.transform.localEulerAngles = curRot;
     }
+
+    public void OnBeginDrag(BaseEventData data)
+    {
+        PointerEventData pointerData = (PointerEventData)data;
+        Vector3 objectScreenPosition = Camera.main.WorldToScreenPoint(this.transform.position);
+        offset = this.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(pointerData.position.x, pointerData.position.y, objectScreenPosition.z));
+    }
+
     public void OnDrag(BaseEventData data)
     {
         PointerEventData pointerData = (PointerEventData)data;
-        Vector2 position = pointerData.position;
-        this.transform.localPosition = position;
+        Vector3 pointerWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(pointerData.position.x, pointerData.position.y, Camera.main.WorldToScreenPoint(this.transform.position).z));
+        this.transform.position = pointerWorldPosition + offset;
     }
 
     public void ModifyClicked()
     {
         hasClicked = !hasClicked;
     }
-
-
 }
