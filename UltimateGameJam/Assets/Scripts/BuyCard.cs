eaf6d5e4ba4ae89795f3d0d79849243d39723ef1
TarikKrestalica <krestalicatarik@gmail.com>
2024-11-20 00:01:59 -0500

    Dom's Assets inside

--- UltimateGameJam/Assets/Scripts/BuyCard.cs
+++ UltimateGameJam/Assets/Scripts/BuyCard.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine.AI;
 
 public class BuyCard : MonoBehaviour
 {
@@ -25,10 +24,6 @@ public class BuyCard : MonoBehaviour
         GameManager.player.GoldAmount -= (uint)cost;
         GameManager.player.SetCurrentGoldAmount( GameManager.player.GoldAmount);
         CreateObject(); 
-
-        // Bake new constraints for AI in runtime.
-        GenerateNavMesh();
-        
         UpdateUI();
     }
 
@@ -40,8 +35,7 @@ public class BuyCard : MonoBehaviour
     void CreateObject()
     {
         GameObject newItem = Instantiate(LoadNeededItem(), Vector3.up, Quaternion.identity, GameObject.FindWithTag("Background").transform);
-        Vector3 modScale = new Vector3(120, 60, 1);
-        newItem.transform.localScale = modScale;
+        FindObjectOfType<NavigationBaker>().objectsToRotate.Add(newItem.transform);
     }
 
     GameObject LoadNeededItem()
@@ -51,36 +45,4 @@ public class BuyCard : MonoBehaviour
         return dup;
     }
 
-    private void GenerateNavMesh()
-{
-    // Use this if you want to clear existing
-    //NavMesh.RemoveAllNavMeshData();  
-    var settings = NavMesh.CreateSettings();
-    var buildSources = new List<NavMeshBuildSource>();
-    // create floor as passable area
-    var floor = new NavMeshBuildSource
-    {
-        transform = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, Vector3.one),
-        shape = NavMeshBuildSourceShape.Box,
-        size = new Vector3(10, 1, 10)
-    };
-    buildSources.Add(floor);
-    
-    // Create obstacle 
-    const int OBSTACLE = 1 << 0;
-    var obstacle = new NavMeshBuildSource
-    {
-        transform = Matrix4x4.TRS(new Vector3(3,0,3), Quaternion.identity, Vector3.one),
-        shape = NavMeshBuildSourceShape.Box,
-        size = new Vector3(1, 1, 1),
-        area = OBSTACLE
-    }; 
-    buildSources.Add(obstacle);
-    
-    // build navmesh
-    NavMeshData built = NavMeshBuilder.BuildNavMeshData(
-        settings, buildSources, new Bounds(Vector3.zero, new Vector3(10,10,10)), 
-        new Vector3(0,0,0), Quaternion.identity);
-    NavMesh.AddNavMeshData(built);
-}
 }
