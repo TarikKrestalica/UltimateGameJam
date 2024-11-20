2d1d63b0e704d95a85350b0b27c6ebc3f278d485
TarikKrestalica <krestalicatarik@gmail.com>
2024-11-20 13:37:50 -0500

    Organization.

--- UltimateGameJam/Assets/Scripts/NavigationBaker.cs
+++ UltimateGameJam/Assets/Scripts/NavigationBaker.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.AI;
 using NavMeshPlus.Components;
 
+// Link to helper video: https://www.youtube.com/watch?v=mV-Uh_FEBn4
+// Link to script: https://gist.githubusercontent.com/iwanPlays/7fdd3cde93adc84d67e28d4d11aa5370/raw/fae00b006b580859da768ebb6900b1fa96a328af/NavigationBaker.cs
 public class NavigationBaker : MonoBehaviour {
 
     public NavMeshSurface[] surfaces;
